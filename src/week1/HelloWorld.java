99563ddcda5e71e7e1695705661fd0af019f2611
velez <velez@msoe.edu>
2026-01-17 14:48:22 -0600

    Initial commit

---
+++ src/week1/HelloWorld.java
@@ -0,0 +1,10 @@
+package week1;
+
+/**
+ * Delete this file to start your assignment.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+    }
+}
