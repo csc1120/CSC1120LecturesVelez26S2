4252961aba871e98c95b76fa51b92cd185144dce
Adela Velez <adela.m.velez@gmail.com>
2026-02-04 13:31:42 -0600

    add the bin example files to week 1 binary example

---
+++ src/week3/lecture/ForEachExample.java
@@ -0,0 +1,29 @@
+/*
+ * Course: SWE2410 - 111, 121
+ * Spring 2026
+ * Lecture Code
+ * Name: Adela Velez
+ * Created: 2/4/2026
+ */
+
+package week3.lecture;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class ForEachExample {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("foo");
+        list.add("bar");
+        list.add("taco");
+        list.add("cat");
+
+        Stream<String> stringStream = list.stream();
+        stringStream.forEach(ForEachExample::printEach);
+    }
+    public static void printEach(String s){
+        System.out.println(s.toUpperCase());
+    }
+}
