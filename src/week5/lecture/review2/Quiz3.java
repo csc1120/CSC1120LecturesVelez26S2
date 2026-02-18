dd147cfe709a7e3f37e7765a78625280b033143d
Adela Velez <adela.m.velez@gmail.com>
2026-02-18 15:18:16 -0600

    Code from in class review

---
+++ src/week5/lecture/review2/Quiz3.java
@@ -0,0 +1,45 @@
+/*
+ * Course: SWE2410 - 111, 121
+ * Spring 2026
+ * Lecture Code
+ * Name: Adela Velez
+ * Created: 2/18/2026
+ */
+
+package week5.lecture.review2;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Quiz3 {
+    Label label = new Label();
+
+    public void changeLabel(ActionEvent event){
+        int value = Integer.parseInt(label.getText());
+        Button b = (Button) event.getSource();
+        if(b.getText().equalsIgnoreCase("Up")){
+            value++;
+        } else {
+            value--;
+        }
+        label.setText(String.valueOf(value));
+
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(5 + 4 + " ");
+
+        List<String> words = Arrays.asList("bear","dog","lion","tiger");
+
+        words.stream().
+                map(a -> a.toUpperCase()).
+                map(a -> a.charAt(0)).
+                forEach(a -> System.out.println(a + ":"));
+
+    }
+}
